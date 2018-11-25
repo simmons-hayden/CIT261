53b4a28688eaac2f29f538a67cc73e9d38c5fdc3
simmons-hayden <haydendsimmons@gmail.com>
2018-11-25 02:03:20 -0700

    Add files via upload

---
+++ objective-10/main.js
@@ -0,0 +1,9 @@
+var audioCanvas = document.getElementById("audioCanvas");
+var audioCanvasText = audioCanvas.getContext("2d");
+audioCanvasText.font = "30px Arial";
+audioCanvasText.fillText("BABY SHARK AUDIO",10,50);
+
+var videoCanvas = document.getElementById("videoCanvas");
+var videoCanvasText = videoCanvas.getContext("2d");
+videoCanvasText.font = "30px Arial";
+videoCanvasText.fillText("BABY SHARK VIDEO",10,50);
